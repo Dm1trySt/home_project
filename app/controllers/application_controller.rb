4f3d2b3984a4c8e1a99adb0ef7c49b6ad0cd94a0
Dmitry <dim139@mail.ru>
2022-05-17 00:56:52 +0300

    update_decorator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,20 @@ class ApplicationController < ActionController::Base
   include ErrorHandling
   include Pagy::Backend
 
+  # Поиск текущего пользователя с помощью гема Devise
+  def devise_current_user
+    @devise_current_user ||= warden.authenticate(scope: :user).decorate
+  end
+
+  # Текущий пользователь
+  def current_user
+    if params[:user_id].blank?
+      devise_current_user
+    else
+      User.find(params[:user_id]).decorate
+    end
+  end
+
   protected
 
   def configure_permitted_parameters
